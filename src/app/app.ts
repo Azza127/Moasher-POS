1536bcb1c85b9f3d3b5a8a60f7e5a26f62875fa7
Azza Ezzat <azzaeldamaty2@gmail.com>
2026-08-26 10:21:23 +0300

    Fix app.ts merge conflicts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,21 +1,11 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-import { RouterOutlet } from '@angular/router';
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
-})
-export class App {}
-=======
 import { Router, RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './shared/components/sidebar/sidebar';
 import { HeaderComponent } from './shared/components/header/header';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [
     RouterOutlet,
     SidebarComponent,
@@ -35,4 +25,3 @@ export class App {
     );
   }
 }
->>>>>>> origin/feature/auth-settings
