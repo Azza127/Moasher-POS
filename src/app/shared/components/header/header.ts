6749656bb05059326a44c1867d402468ac4691a1
Azza Ezzat <azzaeldamaty2@gmail.com>
2026-09-01 01:19:05 +0300

    feat: Updates system UI

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { PopupService } from '../../../core/services/popup.service';
+import { StoreSettingsService } from '../../../core/services/store-settings.service';
+import { StoreSettings } from '../../../core/models/store-settings.model';
 
 @Component({
   selector: 'app-header',
@@ -8,13 +11,21 @@ import { Router } from '@angular/router';
   styleUrl: './header.css'
 })
 export class HeaderComponent {
-
+  settings!: StoreSettings;
   userName = 'Store Owner';
   userRole = 'Owner';
 
   constructor(
-    private router: Router
+    private router: Router,
+    private readonly popupService: PopupService,
+    private readonly settingService: StoreSettingsService
   ) {
+
+    this.settingService.getSettings().subscribe({
+      next: (settings) => {
+        this.settings = settings[0];
+      }
+    });
 
     const user = localStorage.getItem('currentUser');
 
@@ -31,18 +42,20 @@ export class HeaderComponent {
 
  logout(): void {
 
-  const confirmed = confirm(
-    'Are you sure you want to logout?'
-  );
-
-  if (!confirmed) {
-    return;
-  }
+this.popupService.showConfirm(
+      'Are you sure you want to logout?',
+      'logout'
+    ).subscribe({
+      next: (confirmed) => {
+        if (!confirmed) {
+          return;
+        }
 
   localStorage.removeItem('currentUser');
   localStorage.removeItem('isLoggedIn');
 
   window.location.href = '/login';
 }
-
+});
+}
 }
